261757eb32cbf5a52d3c7fc88b51202ee07d9f7f
MAC <mac@LINZO.local>
2022-11-12 15:03:37 +0000

    Test unitaire

---
+++ BanqueTest/CompteBancaireTests.cs
@@ -0,0 +1,45 @@
+﻿using CompteBanqueNS;
+
+namespace BanqueTests;
+
+
+[TestClass]
+public class CompteBancaireTests
+{
+    [TestMethod]
+    public void VerifierDebitCompteCorrect()
+    {
+        double soldeInitial = 500000;
+        double montantDebit = 400000;
+        double soldeAttendu = 100000;
+
+        CompteBancaire compte = new CompteBancaire("Pr. Alioune Sall", soldeInitial);
+
+        compte.Debiter(montantDebit);
+
+        double soldeObtneu = compte.Balance;
+        Assert.AreEqual(soldeAttendu, soldeObtneu, 0.001, "Le solde attendu n'est celui obtenu");
+    }
+
+    [TestMethod]
+    [ExpectedExceptionAttribute(typeof(ArgumentOutOfRangeException))]
+    public void DebiterMontantSuperieurSoldeLeveArgumentOutOfRange() { 
+        double soldeInitial = 500000;
+        double montantDebit = 700000;
+
+        CompteBancaire compte = new CompteBancaire("Pr. Alioune Sall", soldeInitial);
+
+        compte.Debiter(montantDebit);
+    }
+    
+    [TestMethod]
+    [ExpectedExceptionAttribute(typeof(ArgumentOutOfRangeException))]
+    public void DebiterMontantNegatifLeveArgumentOutOfRange() { 
+        double soldeInitial = 500000;
+        double montantDebit = -200000;
+
+        CompteBancaire compte = new CompteBancaire("Pr. Alioune Sall", soldeInitial);
+
+        compte.Debiter(montantDebit);
+    }
+}
